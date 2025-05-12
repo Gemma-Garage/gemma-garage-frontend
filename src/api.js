f2b42c0d0ad320d1f4b8dbb89977f48a06cd557c
Lucas-Fernandes-Martins <lucasfmartins16@gmail.com>
2025-05-11 23:20:42 -0300

    fixing url and adding sample dataset link

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
 // // src/api.js
-export const API_BASE_URL = process.env.REACT_APP_API_URL || " https://llm-garage-513913820596.us-central1.run.app ";
+export const API_BASE_URL = process.env.REACT_APP_API_URL || "https://llm-garage-513913820596.us-central1.run.app";
 export const WS_BASE_URL = process.env.REACT_APP_WS_URL || "ws://llm-garage-513913820596.us-central1.run.app ";
 // src/api.js
 // export const API_BASE_URL = process.env.REACT_APP_API_URL || "https://llm-garage-api-513913820596.us-central1.run.app";
