8a4829775f80e0b1fcfb73a4cc99cdc059b6c899
Lucas-Fernandes-Martins <lucasfmartins16@gmail.com>
2025-06-10 20:34:30 -0300

    adding inference

--- src/api.js
+++ src/api.js
@@ -1,7 +1,8 @@
 // // src/api.js
 export const API_BASE_URL = process.env.REACT_APP_API_URL || "https://llm-garage-513913820596.us-central1.run.app";
+export const API_INFERENCE_BASE_URL = process.env.REACT_APP_API_URL || "https://llm-garage-inference-513913820596.us-central1.run.app";
 export const WS_BASE_URL = process.env.REACT_APP_WS_URL || "wss://llm-garage-513913820596.us-central1.run.app";
-// src/api.js
+// src/api.j
 // export const API_BASE_URL = process.env.REACT_APP_API_URL || "https://llm-garage-api-513913820596.us-central1.run.app";
 // export const WS_BASE_URL = process.env.REACT_APP_WS_URL || "wss://llm-garage-api-513913820596.us-central1.run.app";
 
