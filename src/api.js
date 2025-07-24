6ccffd2c3a88508d71803df68ffab6d77212ef40
Lucas-Fernandes-Martins <lucasfmartins16@gmail.com>
2025-07-24 15:59:51 -0300

    fixing inference

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
 // // src/api.js
 export const API_BASE_URL = process.env.REACT_APP_API_URL || "https://llm-garage-513913820596.us-central1.run.app";
-export const API_INFERENCE_BASE_URL = process.env.REACT_APP_API_INFERENCE_URL || "https://llm-garage-inference-513913820596.us-central1.run.app";
+export const API_INFERENCE_BASE_URL = process.env.REACT_APP_API_INFERENCE_URL || "https://llm-garage-finetuning-513913820596.us-central1.run.app";
 export const WS_BASE_URL = process.env.REACT_APP_WS_URL || "wss://llm-garage-513913820596.us-central1.run.app";
 
